877fc9ef20851d114e27040cbac8a515ae7aa07b
natalypessoa <nanappcruz@gmail.com>
2026-06-05 11:40:16 -0300

    Update CriancaMatriculada.java

--- src/modelo/CriancaMatriculada.java
+++ src/modelo/CriancaMatriculada.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Nataly
- */
-public class CriancaMatriculada {
+public class CriancaMatriculada extends Crianca{
+    private String curso;
+    private String turma;
+    private String turno;
     
+    public CriancaMatriculada() {
+        super();
+    }
+    
+    public CriancaMatriculada(String nome, String cpf, String turma,
+                               int idade, String responsavel, String curso, String turno) {
+        super(nome, cpf, idade, responsavel);
+        this.curso = curso;
+        this.turma = turma;
+        this.turno = turno;
+    }
+
+    public CriancaMatriculada(int id) {
+        super(id);
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + " | Curso: " + curso
+                + "| Turma: "+ turma
+                + "| Turno:"+ turno;
+    }
 }
+    
