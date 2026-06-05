9bdac70b89c8cac190ad2328de9eb8b340396da6
natalypessoa <nanappcruz@gmail.com>
2026-06-05 12:14:49 -0300

    Refactor Crianca class to extend Pessoa class

--- src/modelo/Crianca.java
+++ src/modelo/Crianca.java
@@ -1,62 +1,59 @@
 
 package modelo;
-
-public class Crianca {
-
-    private static int contadorId = 0;
-
-    private int id;
-    private String nome;
-    private String cpf;
-    private int idade;
-    private String genero;
-    private String responsavel;
-
-    // Construtor padrao
-    public Crianca() {
-        this.id = ++contadorId;
-    }
-
-    // Construtor com todos os atributos exceto id
-    public Crianca(String nome, String cpf, int idade, String responsavel) {
-        this.id = ++contadorId;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.idade = idade;
-        this.responsavel = responsavel;
-    }
-
-    // Construtor apenas com id (para pesquisa)
-    public Crianca(int id) {
-        this.id = id;
-    }
-
-    public int getId()               { return id; }
-    public void setId(int id)        { this.id = id; }
-
-    public String getNome()          { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-
-    public String getCpf()           { return cpf; }
-    public void setCpf(String cpf)   { this.cpf = cpf; }
-
-
-    public int getIdade()            { return idade; }
-    public void setIdade(int idade)  { this.idade = idade; }
-
-    public String getGenero() { return genero; }
-    public void setGenero(String genero) { this.genero = genero; }
+ 
+public class Crianca extends Pessoa {
+ 
+    private String turma;
+    private String turno;
+    private String curso;
     
-    public String getResponsavel()               { return responsavel; }
-    public void setResponsavel(String responsavel) { this.responsavel = responsavel; }
+ 
+    public Crianca() {
+        super();
+    }
 
+    public Crianca(String nome, String cpf, int idade,
+                   String turma, String turno, String curso) {
+        super(nome, cpf, idade);
+        this.turma = turma;
+        this.turno = turno;
+        this.curso = curso;
+
+    }
+ 
+    public Crianca(int id) {
+        super(id);
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
     @Override
     public String toString() {
-        return "|ID: " + id
-             + " | Nome: " + nome
-             + " | CPF: " + cpf
-             + " | Idade: " + idade
-             + " | Genero: " + genero
-             + " | Responsavel: " + responsavel;
+        return super.toString()
+                + " | Turma: " + turma
+                + " | Turno: " + turno
+                + "| Curso: " + curso;
     }
-}  
+}
