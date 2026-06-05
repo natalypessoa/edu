4be318b0a88a2a29de9cce2c845bc28fda914fbd
natalypessoa <nanappcruz@gmail.com>
2026-06-05 12:15:44 -0300

    Implement Responsavel class extending Pessoa
    Added constructors and overridden toString method for Responsavel class.

--- src/modelo/Responsavel.java
+++ src/modelo/Responsavel.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Nataly
- */
-public class Responsavel {
-    
+public class Responsavel extends Pessoa {
+
+    // Construtor padrão
+    public Responsavel() {
+        super();
+    }
+
+    // Construtor completo (sem id)
+    public Responsavel(String nome, String cpf, int idade) {
+        super(nome, cpf, idade);
+    }
+
+    // Construtor só com id (para pesquisa)
+    public Responsavel(int id) {
+        super(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Responsavel{" + super.toString() + '}';
+    }
 }
