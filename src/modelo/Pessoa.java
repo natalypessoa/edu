f3c9bd86439cafa1f4056a1e9b7fc9629c697869
Mariana Ribeiro <wrwkm67nht@privaterelay.appleid.com>
2026-06-05 13:08:35 -0300

    Add Email and telefone fields to Pessoa class

--- src/modelo/Pessoa.java
+++ src/modelo/Pessoa.java
@@ -8,18 +8,22 @@ public class Pessoa {
     private String nome;
     private String cpf;
     private int idade;
-
+    private  String Email;
+    private  int telefone;
+    
     // Construtor padrão
     public Pessoa() {
         this.id = ++contador;
     }
 
     // Construtor completo (sem id)
-    public Pessoa(String nome, String cpf, int idade) {
+    public Pessoa(String nome, String cpf, int idade, string Email, int telefone) {
         this.id = ++contador;
         this.nome = nome;
         this.cpf = cpf;
         this.idade = idade;
+        this.Email= Email;
+        this.telefone = telefone 
     }
 
     // Construtor só com id (para pesquisa)
@@ -55,13 +59,28 @@ public class Pessoa {
     public void setIdade(int idade) {
         this.idade = idade;
     }
+     public string getEmail() {
+         return Emial;
+     }
+     public void setEmail() {
+         this.Email=Email;
+     }
+    public int gettelefone() {
+        return telefone;
+    }
+    public void settelefone(int telefone) {
+        this.telefone= telefone;
+    }
 
+ 
     @Override
     public String toString() {
         return "id=" + id +
                ", nome='" + nome + '\'' +
                ", cpf='" + cpf + '\'' +
-               ", idade=" + idade;
+               ", idade=" + idade + '\'' +
+               ", Email=" + Email + '\'' +
+               ", telefone=" + telefone ;
     }
 }
 
