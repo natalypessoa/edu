713dfdf34be2a6a5709c8a52f9d808e5728666e8
pedro <gordinho.fujao@gmail.com>
2026-06-06 02:51:14 -0300

    Update Pessoa.java

--- src/modelo/Pessoa.java
+++ src/modelo/Pessoa.java
@@ -1,45 +1,29 @@
 package modelo;
 
-public class Pessoa {
+public abstract class Pessoa {
 
-    private static int contador = 0;
-
-    private int id;
+    private String gmail;
     private String nome;
     private String cpf;
     private int idade;
-    private  String email;
-    private  int telefone;
+   
     
     // Construtor padrão
-    public Pessoa() {
-        this.id = ++contador;
-    }
-
-    // Construtor completo (sem id)
-    public Pessoa(String nome, String cpf, int idade, String email, int telefone) {
-        this.id = ++contador;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.idade = idade;
-        this.email = email;
-        this.telefone = telefone; 
-    }
-
-    // Construtor só com id (para pesquisa)
-    public Pessoa(int id) {
-        this.id = id;
-    }
-
-    // Getters e Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public Pessoa(String nome, String cpf, int idade,String gmail) {
+    this.nome = nome;
+    this.gmail = gmail;
+    this.cpf = cpf;
+    this.idade = idade;
     }
     
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
 
     public String getNome() {
         return nome;
@@ -64,28 +48,13 @@ public class Pessoa {
     public void setIdade(int idade) {
         this.idade = idade;
     }
-     public String getEmail() {
-         return email;
-     }
-     public void setEmail( String email) {
-         this.email= email;
-     }
-    public int gettelefone() {
-        return telefone;
-    }
-    public void settelefone(int telefone) {
-        this.telefone= telefone;
-    }
 
  
-    @Override
-    public String toString() {
-        return "| Id: " + id +
-               "| Nome: '" + nome + '\'' +
-               "| Cpf: " + cpf + '\'' +
-               "| Idade: " + idade + '\'' +
-               "| Email: " + email + '\'' +
-               "| Telefone: " + telefone ;
-    }
+    
+@Override
+    public String toString() {return nome + " CPF: " + cpf +" IDADE: " + idade+" GMAIL: "+gmail;}
 }
+
+    
+
     
